a65488d0965a4b8ee57180936cc16bad8de0c9b9
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-28 17:28:49 -0700

    onClick Event and dummy like jsx

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -3,10 +3,17 @@ import React, { Component } from "react";
 // Input: Liked: boolean
 // Output: OnClick
 
-class Like extends Component {
-  render() {
-    return <i className="fa fa-heart-o" aria-hidden="true"></i>;
-  }
-}
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
+  return (
+    <i
+      style={{ cursor: "pointer" }}
+      onClick={props.onClick}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 
 export default Like;
