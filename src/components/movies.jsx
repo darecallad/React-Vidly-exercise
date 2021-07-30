13b4ff7f08be9ab63d52ce42f615a308715f8c37
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-30 09:55:15 -0700

    Sorting- Rising the Sorting event

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -15,7 +15,7 @@ class Movies extends Component {
   };
 
   componentDidMount() {
-    const genres = [{ name: "All Genres" }, ...getGenres()];
+    const genres = [{_id="", name: "All Genres" }, ...getGenres()];
 
     this.setState({ movies: getMovies(), genres });
   }
@@ -40,6 +40,10 @@ class Movies extends Component {
   handleGenreSelect = (genre) => {
     this.setState({ selectedGenre: genre, currentPage: 1 });
   };
+
+  handleSort = (path) =>{
+
+  }
 
   render() {
     const { length: count } = this.state.movies;
@@ -74,6 +78,7 @@ class Movies extends Component {
             movies={movies}
             onLike={this.handleLike}
             onDelete={this.handleDelete}
+            onSort={this.handleSort}
           />
           <Pagination
             itemsCount={filtered.length}
