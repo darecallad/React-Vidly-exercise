8afcbeccc0576380563b9f2b9d493078901bed85
darecallad <Ashane199080706@hotmail.com.tw>
2021-07-30 12:27:29 -0700

    Sorting-Extracting Table

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import TableHeader from "./tableHeader";
+import Table from "./table";
 import Like from "./like";
-import TableBody from "./tableBody";
 
 class MoviesTable extends Component {
   columns = [
@@ -32,14 +31,12 @@ class MoviesTable extends Component {
     const { movies, sortColumn, onSort } = this.props;
 
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        onSort={onSort}
+        sortColumn={sortColumn}
+      />
     );
   }
 }
