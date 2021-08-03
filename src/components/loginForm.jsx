b09e98e87f3bbc789acfdb44cd61dbec39e6fb99
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-03 12:05:17 -0700

    display validation errors

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -8,12 +8,22 @@ class LoginForm extends Component {
   };
   //   username = React.createRef();
 
+  validate = () => {
+    const errors = {};
+    const { account } = this.state;
+    if (account.username.trim() === "")
+      errors.username = "Username is required.";
+    if (account.password.trim() === "")
+      errors.password = "Password is required.";
+
+    return Object.keys(errors).length === 0 ? null : errors;
+  };
+
   handleSubmit = (e) => {
     e.preventDefault();
 
     const errors = this.validate();
-    console.log(errors);
-    this.setState({ errors });
+    this.setState({ errors: errors || {} });
     if (errors) return;
     // call the server save the change and rediect user to page
     const username = this.username.current.value;
@@ -29,7 +39,7 @@ class LoginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
     return (
       <div>
         <h1>Log in</h1>
@@ -39,12 +49,14 @@ class LoginForm extends Component {
             value={account.username}
             label="Username"
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name="password"
             value={account.password}
             label="Password"
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className="btn btn-primary">Login</button>
