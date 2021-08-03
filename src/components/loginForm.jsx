29936549a22e2b5641b5bae9049c7a70961731d5
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-03 09:24:55 -0700

    hnadling for submit

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // call the server save the change and rediect user to page
+
+    console.log("Submited");
+  };
+
   render() {
     return (
       <div>
         <h1>Log in</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input id="username" type="text" className="form-control" />
