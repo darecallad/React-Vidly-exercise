272e7fe98c734698176d419f6c0ef82cb9e90367
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-09 16:13:07 -0700

    config.json

--- src/services/GenreService.js
+++ src/services/GenreService.js
@@ -1,6 +1,8 @@
 import http from "./httpService";
-
+import config from "../config.json";
+// import { apiUrl } from "../config.jason";
 export function getGenres() {
   //   return http.get();
+  return http.get(config.apiUrl + "/genres");
   //  store on config.js file with link
 }
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,13 +1,13 @@
 import http from "./httpService";
+import { apiUrl } from "../config.json";
 
-const apiEndpoint = "";
-
+const apiEndpoint = apiUrl + "/movies";
 export function deleteMovie(movieId) {
   return http.delete(apiEndpoint + "/" + movieId);
 }
 
 export function getMovies() {
-  return;
+  return http.get(apiEndpoint);
 
   //   return http.get('http://)
   //   http.get ( from config.js)
