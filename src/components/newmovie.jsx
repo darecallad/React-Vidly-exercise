cd28b92e712bb9f595ed17ca23373cc4dd7dc4c8
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-09 17:15:11 -0700

    refactoring

--- src/components/newmovie.jsx
+++ src/components/newmovie.jsx
@@ -31,20 +31,26 @@ class NewMovie extends Form {
       .label("Rate"),
   };
 
-  async componentDidMount() {
+  async populateGenres() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovies() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
+
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+  async componentDidMount() {
+    await this.populateGenres();
+    await this.populateMovies();
   }
 
   mapToViewModel(movie) {
