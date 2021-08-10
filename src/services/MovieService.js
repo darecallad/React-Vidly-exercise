b98be0ed1142745346fad32a7b0a14f6223b131c
darecallad <Ashane199080706@hotmail.com.tw>
2021-08-09 17:23:46 -0700

    Refactoring

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -2,19 +2,23 @@ import http from "./httpService";
 import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
+
+function movieUrl(id) {
+  return `${apiEndpoint}/${id}`;
+}
 export function deleteMovie(movieId) {
-  return http.delete(apiEndpoint + "/" + movieId);
+  return http.delete(movieUrl(movieId));
 }
 
 export function getMovie(movieId) {
-  return http.get(apiEndpoint + "/" + movieId);
+  return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
   if (movie._id) {
     const body = { ...movie };
     delete body._idl;
-    http.put(apiEndpoint + "/" + movie._id, body);
+    http.put(movieUrl(movie._id), body);
   }
 
   return http.post(apiEndpoint, movie);
